c95c6fe709bd354abae1787d69d7887ff2816ed9
luise <luisebrandenburger@gmail.com>
2022-01-10 18:12:27 +0100

    added post request to add user to db

--- client/src/start.js
+++ client/src/start.js
@@ -1,22 +1,21 @@
 import ReactDOM from "react-dom";
-// import HelloWorld from "../../_infos_/helloWorld";
 import Welcome from "./welcome";
-// import WelcomeLoggedIn from "./welcomeLoggedIn";
+import WelcomeLoggedIn from "./welcomeLoggedIn";
 
 
-ReactDOM.render(<Welcome />, document.querySelector("main"));
+// ReactDOM.render(<Welcome />, document.querySelector("main"));
 
-// fetch("/user/id.json")
-//     .then((response) => response.json())
-//     .then((data) => {
-//         if (!data.userId) {
-//             ReactDOM.render(<Welcome />, document.querySelector("main"));
-//         } else {
-//             ReactDOM.render(<WelcomeLoggedIn />, document.querySelector("main"));
+fetch("/user/id.json")
+    .then((response) => response.json())
+    .then((data) => {
+        if (!data.userId) {
+            ReactDOM.render(<Welcome />, document.querySelector("main"));
+        } else {
+            ReactDOM.render(<WelcomeLoggedIn />, document.querySelector("main"));
 
-//             // ReactDOM.render(
-//             //     <img src="/logo.gif" alt="logo" />,
-//             //     document.querySelector("main")
-//             // );
-//         }
-//     });
+            // ReactDOM.render(
+            //     <img src="/logo.gif" alt="logo" />,
+            //     document.querySelector("main")
+            // );
+        }
+    });
--- server/server.js
+++ server/server.js
@@ -3,12 +3,10 @@ const app = express();
 const compression = require("compression");
 const path = require("path");
 const { registerUser } = require("./sql/db");
-const { compare, hash } = require("./bc");
+const { hash } = require("./bc");
 const cookieSession = require("cookie-session");
 let secret =
     process.env.COOKIE_SECRET || require("./secret.json").COOKIE_SECRET;
-
-
 
 /*************************** MIDDLEWARE ***************************/
 
@@ -20,8 +18,6 @@ if (process.env.NODE_ENV == "production") {
         res.redirect(`https://${req.hostname}${req.url}`);
     });
 }
-
-app.use(express.json());
 
 app.use(compression());
 
@@ -35,55 +31,59 @@ app.use(
     })
 );
 
-
+app.use(express.json());
 /*************************** ROUTES ***************************/
 
 app.get("/user/id.json", function (req, res) {
     // TODO: set up cookie middleware
 
-    // res.json({
-    //     userId: req.session.userId,
-    // });
-
-    // FIXME: DEMO - DELETE LATER !!!!!!!!!!!!!!!
-    res.json({
-        userId: undefined,
-    });
+    if (req.session.userId) {
+        res.json({
+            userId: req.session.userId,
+        });
+    } else {
+        res.json({
+            userId: undefined,
+        });
+    }
 });
 
-app.post("/registration.json", (req, res) => {
-    const data = req.body;
-    const pw = data.password;
+app.post("/register.json", (req, res) => {
     console.log("req.body in registration.json request: ", req.body);
 
-    if (data.first == "" || data.last == "" || data.email == "" || pw == "") {
-        console.log("no data in input -> change state to error");
-    } else {
-        hash(pw)
-            .then((hashedPw) => {
-                registerUser(data.first, data.last, data.email, hashedPw)
-                    .then(({ rows }) => {
-                        req.session.userId = rows[0].id;
-                        // TODO: render user Logged in
-                        console.log("console.log req.session: ", req.session);
-                    })
-                    .catch((err) => {
-                        console.log("error adding user: ", err);
-                        console.log("change state to error");
-                    });
-            })
-            .catch((err) => {
-                console.log("err in hash", err);
-            });
-    }
+    const data = req.body;
+    const pw = data.password;
+
+    // if (data.first == "" || data.last == "" || data.email == "" || pw == "") {
+    //     console.log("no data in input -> change state to error");
+    //     res.json({ success: false });
+    // } else {
+    hash(pw)
+        .then((hashedPw) => {
+            registerUser(data.first, data.last, data.email, hashedPw)
+                .then(({ rows }) => {
+                    req.session.userId = rows[0].id;
+                    console.log("console.log req.session: ", req.session);
+                    res.json({ success: true });
+                })
+                .catch((err) => {
+                    console.log("error adding user: ", err);
+                    console.log("change state to error");
+                    res.json({ success: false });
+                });
+        })
+        .catch((err) => {
+            console.log("err in hash", err);
+            res.json({ error: true });
+        });
+    // }
 });
 
 app.get("/logout", (req, res) => {
     req.session = null;
     // TODO:  render Loged Out Component
+    res.json({ loggedIn: false });
 });
-
-
 
 // ************************* ANY ROUTS ABOVE ******************************
 
