17edeb2779dc1ce29e7750589a3c2a0ea479f2af
luise <luisebrandenburger@gmail.com>
2022-01-11 17:37:05 +0100

    added renderStage function to reset Component

--- server/server.js
+++ server/server.js
@@ -125,7 +125,41 @@ app.post("/login.json", (req, res) => {
 
 // ************************* RESET PASSWORD ROUTS ******************************
 
+app.post("/password/reset/start", (req, res) => {
+    console.log("req.body in login.json request: ", req.body);
 
+    // const data = req.body;
+    // const pw = data.password;
+
+    // if (req.session.userId) {
+    //     console.log("user already has a session Id");
+    // } else {
+    //     console.log("user doesnt have a session id");
+    // }
+
+    // getUserByEmail(data.email)
+    //     .then(({ rows }) => {
+    //         compare(pw, rows[0].password)
+    //             .then((match) => {
+    //                 if (match) {
+    //                     req.session.userId = rows[0].id;
+    //                     console.log("console.log req.session: ", req.session);
+    //                     res.json({ success: true });
+    //                 } else {
+    //                     console.log("Error in Match");
+    //                     res.json({ success: false });
+    //                 }
+    //             })
+    //             .catch((err) => {
+    //                 console.log("password error", err);
+    //                 res.json({ success: false });
+    //             });
+    //     })
+    //     .catch((err) => {
+    //         console.log("error finding user: ", err);
+    //         res.json({ success: false });
+    //     });
+});
 
 
 
